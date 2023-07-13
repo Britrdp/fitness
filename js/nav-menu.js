025dd635a10ca275760496be673fbea87edf02b0
Britrdp <118445129+Britrdp@users.noreply.github.com>
2023-07-13 21:18:17 +0200

    Add files via upload

---
+++ js/nav-menu.js
@@ -0,0 +1,35 @@
+var div = document.createElement("div");
+
+div.innerHTML = `<div class="menu-container">
+<div class="menu">
+    <ul>
+      <li class='menu-logo desktop'><a href="index.html"><img src="images/Tate-Logo-Web3.png" alt="Tate logo" /></a></li>
+      <li class='mobile'><a href="index.html">HOME</a></li>
+      <li><a href="index.html" target='_blank'>HOME</a></li>
+      <li><a href="official.html" target="_blank">SOCIAL MEDIA</a></li>
+      <li><a href='newsletter.html'>CONTACTE ME</a>
+      
+      
+      <li><a href=""></a></li>
+      <li class='right-btn shop-btn'><a href="shop.html">SHOP</a></li>
+      <li class='right-btn shop-btn'><a href="">EXCLUSIVE</a></li>
+    </ul>
+</div>
+</div>`;
+
+document.currentScript.parentNode.insertBefore(
+  div.firstElementChild,
+  document.currentScript
+);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  $('head').append('<link rel="stylesheet" href="css/menu-style.css" type="text/css"/>');
+  $('head').append('  <link rel="stylesheet" href="css/ionicons.min.css" type="text/css">');
+  
+  $("#agent-btn").click(function(e) {
+    Intercom('showNewMessage');
+  });
+});
+
