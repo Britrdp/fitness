190d14cc2d4f7ab3d9289e0cd90fe8f9af0687f1
Britrdp <118445129+Britrdp@users.noreply.github.com>
2023-07-18 19:59:00 +0200

    Update nav-menu.js

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -3,7 +3,7 @@ var div = document.createElement("div");
 div.innerHTML = `<div class="menu-container">
 <div class="menu">
     <ul>
-      <li class='menu-logo desktop'><a href="index.html"><img src="images/Tate-Logo-Web3.png" alt="Tate logo" /></a></li>
+      <li class='menu-logo desktop'><a href="index.html"><img src=https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjCIPrIw8BwFESUO1J7f17FOnSJX87QC1nhZ3IOWkunVbs4yLu2sIELIN_CtDtBopoyChDaBB-ZJ68cCnA4z_VA9bcT5q625vneiK63NCTCMUc5l5_NY_6_4sba9aw8xy3CaRvQyiq4BnD1cbGnVQ_A0fWZoFiz8TRJ9P9B1EpDxEyFoWdQqh-yWVX0QUx_/s320/w.pngg" alt="Tate logo" /></a></li>
       <li class='mobile'><a href="index.html">HOME</a></li>
       <li><a href="index.html" target='_blank'>HOME</a></li>
       <li><a href="official.html" target="_blank">SOCIAL MEDIA</a></li>
